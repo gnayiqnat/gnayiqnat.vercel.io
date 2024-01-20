53d4d41d05ae0884b6adbbfdfa8b2be405c1ab83
gnayiqnat <gnayiqnat@gmail.com>
2024-01-20 17:20:34 +0800

    Add transition animation in homepage on page load

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,12 +1,14 @@
 // components
+import { Box, duration } from '@mui/material';
 import BackgroundTypography from './components/BackgroundTypography';
 import Header from './components/Header';
 import IntroductionSection from './components/IntroductionSection';
 import WorksSection from './components/WorksSection';
+import { motion } from 'framer-motion';
 
 export default function Homepage() {
 	return (
-		<>
+		<motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5, duration: 0.75}}>
 			<Header />
 
 			<IntroductionSection />
@@ -16,6 +18,6 @@ export default function Homepage() {
 \			
 			{/* Backdrop */}
 			<BackgroundTypography />
-		</>
+		</motion.div>
 	);
 }
