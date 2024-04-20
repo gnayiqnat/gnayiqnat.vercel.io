193f0d884f7c3f38b1c333250d95af3c4f2896b7
gnayiqnat <gnayiqnat@duck.com>
2024-04-20 10:42:51 +0800

    WorksSection - Replaced repetitive code with Loop/Map

--- src/components/WorksSection.jsx
+++ src/components/WorksSection.jsx
@@ -4,21 +4,24 @@ import { Grid, Card, Typography, Box } from '@mui/material';
 import digitalascentPeek from '/digitalascent-peek.png';
 import { motion, useScroll } from 'framer-motion';
 function WorksSection() {
-	const MotionCard = motion(Card);
+    const MotionCard = motion(Card);
 
-	return (
-		<section id='sec-2'>
-			<Grid
-				container
-				sx={{
-					margin: 'auto',
-					marginTop: 10,
-					marginBottom: -3,
-					padding: 3,
-					height: '100dvh',
-				}}
-			>
-				{/* 
+    const listofWorks = [
+        { url: 'https://digitalascent.space', img: digitalascentPeek },
+    ];
+    return (
+        <section id='sec-2'>
+            <Grid
+                container
+                sx={{
+                    margin: 'auto',
+                    marginTop: 10,
+                    marginBottom: -3,
+                    padding: 3,
+                    height: '100dvh',
+                }}
+            >
+                {/* 
 				<Typography
 					variant='h3'
 					sx={{ color: '#c60054', fontWeight: 600, marginBottom: '30px' }}
@@ -26,51 +29,55 @@ function WorksSection() {
 					My Works
 				</Typography>
 				 */}
-				<Grid
-					container
-					sx={{
-						display: 'flex',
-						flexDirection: 'column',
-						height: '80dvh',
-					}}
-				>
-					<Grid item></Grid>
-					<Grid item>
-						<MotionCard
-							whileHover={{ borderColor: '#c60054', opacity: 1 }}
-							variant='outlined'
-							sx={{
-								backgroundColor: 'transparent',
-								borderColor: '#550024',
-								opacity: 0.6,
-								padding: 1.5,
-								borderRadius: 3,
-								width: '330px',
-								maxWidth: '80vw',
-							}}
-						>
-							<a
-								draggable='false'
-								target='_blank'
-								href='https://digitalascent.space'
-								style={{ width: '330px', maxWidth: '80vw' }}
-							>
-								<img
-									draggable='false'
-									style={{
-										width: '330px',
-										maxWidth: '80vw',
-										borderRadius: '7px',
-									}}
-									src={digitalascentPeek}
-								/>
-							</a>
-						</MotionCard>{' '}
-					</Grid>
-				</Grid>
-			</Grid>
-		</section>
-	);
+                <Grid
+                    container
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '80dvh',
+                    }}
+                >
+                    {listofWorks.map((e, i) => 
+                        <Grid item key={i}>
+                            <MotionCard
+                                whileHover={{
+                                    borderColor: '#c60054',
+                                    opacity: 1,
+                                }}
+                                variant='outlined'
+                                sx={{
+                                    backgroundColor: 'transparent',
+                                    borderColor: '#550024',
+                                    opacity: 0.6,
+                                    padding: 1.5,
+                                    borderRadius: 3,
+                                    width: '330px',
+                                    maxWidth: '80vw',
+                                }}
+                            >
+                                <a
+                                    draggable='false'
+                                    target='_blank'
+                                    href={e.url}
+                                    style={{ width: '330px', maxWidth: '80vw' }}
+                                >
+                                    <img
+                                        draggable='false'
+                                        style={{
+                                            width: '330px',
+                                            maxWidth: '80vw',
+                                            borderRadius: '7px',
+                                        }}
+                                        src={e.img}
+                                    />
+                                </a>
+                            </MotionCard>
+                        </Grid>
+                    )}
+                </Grid>
+            </Grid>
+        </section>
+    );
 }
 
 export default WorksSection;
